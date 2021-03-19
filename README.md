33b92611d4c9be4c16de0030a0345ae24bf5c7f8
Roger Chi <goblin-qyz@163.com>
2021-03-19 14:45:26 +0800

    feat: finish ver 1.0.0

--- README.md
+++ README.md
@@ -1,70 +1,32 @@
-# f2pool-watcher README
+# f2pool-watcher
 
-This is the README for your extension "f2pool-watcher". After writing up a brief description, we recommend including the following sections.
+This is a VS Code extension that can show [F2Pool](https://www.f2pool.com/) mining informations in the status bar.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+![feature](assets/feature.png)
 
-For example if there is an image subfolder under your extension project workspace:
+1. Displays value, hashrate and worker information of a specified F2Pool currency and username.
+2. Supports automatic refresh at a specified interval
 
-\!\[feature X\]\(images/feature-x.png\)
+## Usage
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+![usage](assets/usage.png)
 
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+1. After installing the plugin, click it to set currency and F2Pool username, if the username is valid, corresponding F2Pool info should show in the status bar.
+2. Click the plugin to manually refresh.
+3. Hover on the plugin to show a tooltip of details.
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+* `f2poolWatcher.currency`: set the currency, currently supports "BTC", "LTC", "ETH"
+* `f2poolWatcher.username`: set the F2Pool username
+* `f2poolWatcher.refreshInterval`: set the automatic refresh interval in minutes, set to 0 to disable auto-refresh, or it must be equal or greater than 5.
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Initial release of f2pool-watch
--- src/extension.ts
+++ src/extension.ts
@@ -15,6 +15,8 @@ const CURRENCIES: { [key: string]: string } = {
   "ETH": "ethereum"
 }
 
+let failed = false
+let interval: NodeJS.Timeout | null = null
 let myStatusBarItem: vscode.StatusBarItem;
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
@@ -22,15 +24,16 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 	const myCommandId = 'f2pool-watcher.update';
 	subscriptions.push(vscode.commands.registerCommand(myCommandId, async () => {
 		const config = vscode.workspace.getConfiguration('f2poolWatcher')
-		if (!config.get('currency')) {
+		if (failed || !config.get('currency')) {
 			const val = await vscode.window.showQuickPick(Object.keys(CURRENCIES), {
 				placeHolder: 'Select a currency',
 			})
 			await config.update('currency', val, true)
 		} 
-		if (!config.get('username')) {
+		if (failed || !config.get('username')) {
 			const val = await vscode.window.showInputBox({
 				placeHolder: 'Enter F2Pool username',
+				value: config.get('username') || '',
 			})
 			await config.update('username', val, true)
 		}
@@ -42,30 +45,67 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 	myStatusBarItem.command = myCommandId;
 	subscriptions.push(myStatusBarItem);
 
+	vscode.workspace.onDidChangeConfiguration(() => {
+		setRefreshInterval()
+		updateStatusBarItem()
+	})
+	
 	// update status bar item once at start
-	updateStatusBarItem();
+	setRefreshInterval()
+	updateStatusBarItem()
 }
 
 function updateStatusBarItem(): void {
 	const config = vscode.workspace.getConfiguration('f2poolWatcher')
 	if (!config || !config.get('currency') || !config.get('username')) {
-		myStatusBarItem.text = `$(ruby) Click to config f2pool-watcher`
+		myStatusBarItem.text = `$(ruby) Click to configure f2pool-watcher`
+		myStatusBarItem.tooltip = undefined
 		myStatusBarItem.show();
 	} else {
-		const url = `https://api.f2pool.com/${CURRENCIES[config.get('currency') as string]}/${config.get('username')}`
-		axios.get<ResF2poolInfo>(url).then(res => {
-			const {
-				value,
-				value_today,
-				hashrate,
-				worker_length,
-				worker_length_online,
-			} = res.data
-			myStatusBarItem.text = 
-				`$(ruby) ETH-rustchi | $(pulse) ${value_today.toFixed(8)}/${value.toFixed(8)} | $(dashboard) ${(hashrate / 1e6).toFixed(2)}MH/s | $(rocket) ${worker_length_online}/${worker_length}`;
-			myStatusBarItem.tooltip = 
-				`ETH-rustchi\ntoday: ${value_today.toFixed(8)}\ntotal: ${value.toFixed(8)}\nhashrate: ${(hashrate / 1e6).toFixed(2)}MH/s\nworkers(online/total): ${worker_length_online}/${worker_length}\n(click to refresh)`;
-			myStatusBarItem.show();
-		})
+		const currency = config.get('currency') as string
+		const currencyUrlSegment = CURRENCIES[currency]
+		const username = config.get('username')
+
+		// query f2pool api
+		myStatusBarItem.text = `$(ruby) Refreshing f2pool-watcher...`
+		myStatusBarItem.tooltip = undefined
+		myStatusBarItem.show();
+		const url = `https://api.f2pool.com/${currencyUrlSegment}/${username}`
+		axios.get<ResF2poolInfo>(url)
+			.then(res => {
+				const {
+					value,
+					value_today,
+					hashrate,
+					worker_length,
+					worker_length_online,
+				} = res.data
+				myStatusBarItem.text = 
+					`$(ruby) ${currency}-${username} | $(pulse) ${value_today.toFixed(8)}/${value.toFixed(8)} | $(dashboard) ${(hashrate / 1e6).toFixed(2)}MH/s | $(rocket) ${worker_length_online}/${worker_length}`;
+				myStatusBarItem.tooltip = 
+					`${currency}-${username}\ntoday: ${value_today.toFixed(8)}\ntotal: ${value.toFixed(8)}\nhashrate: ${(hashrate / 1e6).toFixed(2)}MH/s\nworkers(online/total): ${worker_length_online}/${worker_length}\n(click to refresh)`;
+				myStatusBarItem.show();
+				failed = false
+			})
+			.catch((e) => {
+				myStatusBarItem.text = `$(ruby) Click to configure f2pool-watcher`
+				myStatusBarItem.tooltip = undefined
+				myStatusBarItem.show();
+				vscode.window.showErrorMessage(`Failed to get F2Pool info, please check the plugin settings and try again.`)
+				failed = true
+			})
+	}
+}
+
+function setRefreshInterval(): void {
+	const config = vscode.workspace.getConfiguration('f2poolWatcher')
+	const refreshInterval = config.get('refreshInterval') as number
+
+	// interval must be longer than 5 minutes to prevent frequent API call
+	if (!!config.get('currency') && !!config.get('username') && refreshInterval >= 5) {
+		interval && clearInterval(interval)
+		interval = setInterval(() => {
+			updateStatusBarItem()
+		}, refreshInterval * 60 * 1000)
 	}
 }
